abb63a866fdcfc23d5f5e7f60e64b7d4de95dafe
jlvanar <jlvanarsdall@miners.utep.edu>
2022-09-19 19:23:05 -0600

    Added simple UI file that echoes input.

---
+++ src/simple_ui.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int simple_echo();
+
+int main(){
+  puts("Thus begins the tokenizer UI: as of yet, it simply echoes the input, unless the input 'x'
+ is received. This particular input will exit the program's loop.");
+  simple_echo();
+  
+  return 0;
+
+  
+}
+
+int simple_echo(){
+  char input[256];
+  fputs(">", stdout);
+  while(fgets(input, 256, stdin)){
+    printf("%s\n", input);
+    if(strcmp(input, "x") == 10){
+      fputs("Alright, later, man.\n", stdout);
+      return 0;
+    }
+    else{
+      fputs(">", stdout);
+    }
+  }
+  return 1;
+}
